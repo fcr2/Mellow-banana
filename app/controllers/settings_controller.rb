43bb11f9ef449a16f244e570b80a970fbd6d0a0a
Fcr2 <fcaellesrubies@gmail.com>
2017-06-12 12:55:10 +0200

    setting changes layout new and edit

--- app/controllers/settings_controller.rb
+++ app/controllers/settings_controller.rb
@@ -3,8 +3,44 @@ class SettingsController < ApplicationController
 	layout "admin"
 
 	def index
-
+		@setting = current_user.setting
+		@user = current_user 
 	end
 
+	def show
+		@setting = Setting.find(params[:id])
+	end
+
+	def new
+		@setting = Setting.new
+	end
+
+	def create
+		@setting = current_user.build_setting(setting_params)
+		if @setting.save
+			redirect_to settings_path
+		else
+			render :new
+		end
+	end
+
+	def edit
+		@setting = Setting.find(params[:id])
+	end
+
+	def update
+		@setting = Setting.find(params[:id])
+		if @setting.update_attributes(setting_params)
+			redirect_to settings_path
+		else
+			render :index
+		end
+	end
+
+	private
+
+	def setting_params
+		params.require(:setting).permit(:language, :country)
+	end
 
 end
--- app/models/setting.rb
+++ app/models/setting.rb
@@ -1,3 +1,10 @@
 class Setting < ApplicationRecord
   belongs_to :user
+
+  validates :country, presence: { message:"Obligatory field"}  
+  validates :language, presence: { message:"Obligatory field"}
+
+  CLASS_LIST = ["Catalinia", "Spain", "France", "Germany", "England", "Denmark", "other"]
+
+  CLASS_LIST1 = ["Catalan", "Spanish", "French", "German", "English", "Danish", "other"]
 end
