1c15a2752804f5ed40aa363808b2f610e67a33c6
Fcr2 <fcaellesrubies@gmail.com>
2017-06-14 13:13:51 +0200

    adminusers login

--- config/routes.rb
+++ config/routes.rb
@@ -1,17 +1,22 @@
 Rails.application.routes.draw do
 
-  devise_for :admin_users, ActiveAdmin::Devise.config
-  ActiveAdmin.routes(self)
-root to: 'statics#index', as: 'home'
+  devise_for :users
 
+  devise_for :admin_users, ActiveAdmin::Devise.config
+  
+  ActiveAdmin.routes(self)
+
+  
   get '/about' => 'statics#about', as: 'about'
   get 'settings/index'
-  devise_for :models
-  devise_for :users
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-# root to: "peliculas#index"
-resources :peliculas
-resources :settings
 
+  devise_for :models
+  
+  	# For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+	# root to: "peliculas#index"
+  resources :peliculas
+  resources :settings
+
+  root to: 'statics#index', as: 'home'
 
 end
